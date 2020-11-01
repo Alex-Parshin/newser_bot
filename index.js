0f849a492ddc88259e662dd86a7328d741a19c2b
Hramov <trykhramov@gmail.com>
2020-11-01 12:34:37 +0300

    Improved index.js

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-'use strict'
+"use strict";
 
-import io from 'socket.io-client'
-import Lifecycle from './lib/core/lifecycle.js'
-import store from './lib/core/state/stateManager'
-import dotenv from 'dotenv'
-dotenv.config()
+import io from "socket.io-client";
+import Lifecycle from "./lib/core/lifecycle.js";
+import store from "./lib/core/state/stateManager";
+import dotenv from "dotenv";
+import { log } from './lib/core/state/stateManager'
 
-const lifecycle = new Lifecycle()
+dotenv.config();
 
-export default function socketManager() {
+const lifecycle = new Lifecycle();
 
-    console.log('Connecting to socket server...')
-
-    const socket = io.connect(`${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`)
-    store.setSocket(socket)
-
-    socket.on('confirm', () => {
-        console.log("Connection approved!")
-    })
-
-    socket.on('startBot', ({ source, pages, url, engines }) => {
-        store.setEngines(engines)
-        lifecycle.start({ source, pages, url, engines })
-    })
-
-    socket.on('addQueryToQueue', ({query, id_request, engine}) => {
-        addQueryToQueue({query, id_request, engine})
-    })
-
-    socket.on('stopBot', () => {
-        lifecycle.stop()
-    })
+try {
+    log('Подключаюсь к сокет-серверу...', 0);
+    const socket = io.connect(
+        `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+    );
+    store.setSocket(socket);
+} catch (err) {
+    log(`Ошибка подключения к сокет серверу: ${err}`, 0)
 }
 
-socketManager()
+socket.on("confirm", () => {
+  log('Connection approved!', 0);
+});
+
+socket.on("startBot", ({ source, pages, url, engines }) => {
+  store.setEngines(engines);
+  lifecycle.start({ source, pages, url, engines });
+});
+
+socket.on("addQueryToQueue", ({ query, id_request, engine }) => {
+  addQueryToQueue({ query, id_request, engine });
+});
+
+socket.on("stopBot", () => {
+  lifecycle.stop();
+});
