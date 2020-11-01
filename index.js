777a25113ce3fbfe1997c115f4b5aa5c84f74b26
Hramov <trykhramov@gmail.com>
2020-11-01 12:41:34 +0300

    Improved index.js

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
 "use strict";
 
 import io from "socket.io-client";
-import Lifecycle from "./lib/core/lifecycle.js";
-import store from "./lib/core/state/stateManager";
 import dotenv from "dotenv";
-import { log } from './lib/core/state/stateManager'
-
 dotenv.config();
 
-const lifecycle = new Lifecycle();
+import lifecycle from "./lib/core/lifecycle.js";
+import store from "./lib/core/state/stateManager";
+import { log } from './lib/core/utils/utils'
 
 try {
+    index()
+} catch (err) {
+    console.log(`Ошибка подключения к сокет серверу: ${err}`, 0)
+}
+
+function index() {
     log('Подключаюсь к сокет-серверу...', 0);
     const socket = io.connect(
         `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
     );
     store.setSocket(socket);
-} catch (err) {
-    log(`Ошибка подключения к сокет серверу: ${err}`, 0)
+
+    socket.on("confirm", () => {
+        log('Connection approved!', 0);
+      });
+      
+      socket.on("startBot", ({ source, pages, url, engines }) => {
+        store.setEngines(engines);
+        lifecycle.start({ source, pages, url, engines });
+      });
+      
+      socket.on("addQueryToQueue", ({ query, id_request, engine }) => {
+        addQueryToQueue({ query, id_request, engine });
+      });
+      
+      socket.on("stopBot", () => {
+        lifecycle.stop();
+      });
 }
-
-socket.on("confirm", () => {
-  log('Connection approved!', 0);
-});
-
-socket.on("startBot", ({ source, pages, url, engines }) => {
-  store.setEngines(engines);
-  lifecycle.start({ source, pages, url, engines });
-});
-
-socket.on("addQueryToQueue", ({ query, id_request, engine }) => {
-  addQueryToQueue({ query, id_request, engine });
-});
-
-socket.on("stopBot", () => {
-  lifecycle.stop();
-});
