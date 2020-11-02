684d9fa434c0fc5a523e14bdabb0d2d8459a4343
hramov <trykhramov@gmail.com>
2020-11-02 13:09:34 +0300

    Migrating

--- index.js
+++ index.js
@@ -8,33 +8,22 @@ import lifecycle from "./lib/core/lifecycle.js";
 import store from "./lib/core/state/stateManager";
 import { log } from './lib/core/utils/utils'
 
-try {
-    index()
-} catch (err) {
-    console.log(`Ошибка подключения к сокет серверу: ${err}`, 0)
-}
+log('Подключаюсь к сокет-серверу...', 0);
+const socket = io.connect(`http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
+socket.emit("who_am_i", 'puppeteer_bot');
 
-function index() {
-    log('Подключаюсь к сокет-серверу...', 0);
-    const socket = io.connect(
-        `${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
-    );
-    store.setSocket(socket);
+store.setSocket(socket);
+// lifecycle.mainQueue()
 
-    socket.on("confirm", () => {
-        log('Connection approved!', 0);
-      });
-      
-      socket.on("startBot", ({ source, pages, url, engines }) => {
-        store.setEngines(engines);
-        lifecycle.start({ source, pages, url, engines });
-      });
-      
-      socket.on("addQueryToQueue", ({ query, id_request, engine }) => {
-        addQueryToQueue({ query, id_request, engine });
-      });
-      
-      socket.on("stopBot", () => {
-        lifecycle.stop();
-      });
-}
+// socket.on("startBot", ({ source, pages, url, engines }) => {
+//     store.setEngines(engines);
+//     lifecycle.start({ source, pages, url, engines });
+// });
+
+// socket.on("addQueryToQueue", ({ query, id_request, engine }) => {
+//     addQueryToQueue({ query, id_request, engine });
+// });
+
+// socket.on("stopBot", () => {
+//     lifecycle.stop();
+// });
