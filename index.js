564e398354e51150920514f8c3f7a8d8f8d531b6
hramov <trykhramov@gmail.com>
2020-11-03 12:47:07 +0300

    Interconnection maintained

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ dotenv.config();
 import lifecycle from "./lib/core/lifecycle.js";
 import store from "./lib/core/state/stateManager";
 
-console.log('Подключаюсь к сокет-серверу...');
-const socket = io.connect(`http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`);
+
+const port = process.env.SERVER_PORT
+const host = process.env.SERVER_HOST
+console.log(`Подключаюсь к сокет-серверу http://${host}:${port}`);
+const socket = io.connect(`http://${host}:${port}`);
 
 socket.emit("who_am_i", 'puppeteer_bot');
 store.setSocket(socket);
+socket.emit('log', 'Ожидаю команды СТАРТ')
 
 lifecycle.mainQueue()
 
@@ -23,3 +27,8 @@ socket.on("startBot", ({ source, pages, url, engines }) => {
 socket.on("stopBot", () => {
     lifecycle.stop();
 });
+
+socket.on('disconnect', () => {
+    console.log('Сервер отключился. Завершение работы')
+    process.exit(0)
+})
