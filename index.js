fba59e305b31f0657d0028496cff5feef47b822f
hramov <trykhramov@gmail.com>
2020-11-09 16:49:05 +0300

    Fixed bugs

--- index.js
+++ index.js
@@ -4,29 +4,36 @@ import io from "socket.io-client";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { log } from './lib/core/utils/utils'
+
 import lifecycle from "./lib/core/lifecycle.js";
 import store from "./lib/core/state/stateManager";
 
 
 const port = process.env.SERVER_PORT
 const host = process.env.SERVER_HOST
-console.log(`Подключаюсь к сокет-серверу http://${host}:${port}`);
+log(`Подключаюсь к сокет-серверу http://${host}:${port}`);
 const socket = io.connect(`http://${host}:${port}`);
 
 socket.emit("who_am_i", 'puppeteer_bot');
 store.setSocket(socket);
-socket.emit('log', 'Ожидаю команды СТАРТ')
+log('Ожидаю команды СТАРТ')
 
 lifecycle.mainQueue()
 
-socket.on("startBot", ({ source, pages, url, engines }) => {
+socket.on("start", ({ source, pages, url, engines }) => {
     store.setEngines(engines);
     lifecycle.start({ source, pages, url, engines });
-});
+})
 
-socket.on("stopBot", () => {
-    lifecycle.stop();
-});
+socket.on('restart', () => {
+    log('Перезагружаю бота')
+    process.exit(0)
+})
+
+socket.on("stop", () => {
+    lifecycle.stop()
+})
 
 socket.on('disconnect', () => {
     console.log('Сервер отключился. Завершение работы')
